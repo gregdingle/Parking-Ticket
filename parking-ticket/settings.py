cbf9e54fe91b1125a1b7c9faceae035bc2475e60
K Mamunur Rashid <mamun1980@gmail.com>
2013-02-21 15:36:01 +0600

    Added app.js, added bootstrap app, and working on time of day function

--- parking-ticket/settings.py
+++ parking-ticket/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'tgis',
+    'bootstrapform',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- statics/js/myjs.js
+++
@@ -1,6 +0,0 @@
-alert('hello world!');
-if (window.navigator.geolocation) {
-	alert('geolocation is active');
-} else {
-	alert('geolocation is not active');
-}
